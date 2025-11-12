9d0360be34f9138522c2068599502154ede2b2b4
Bakhtimes <ailpokabdibek@gmail.com>
2025-11-12 14:57:04 +0500

    Updating WeatherSearch functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import './App.css'
-
-function App() {
+import { getWeatherData } from "./services/weatherService";
+import { WeatherSearch } from "./components/ui/WeatherSearch";
+import { useState } from "react";
+import type { WeatherData } from "./types/weather";
+export function App() {
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
   return (
-    <>
-      
-    </>
+    <div className="flex justify-center flex-col">
+      <WeatherSearch 
+        onSearch={async (city) => {
+          const data = await getWeatherData(city)
+          setWeatherData(data)
+          await new Promise(resolve => setTimeout(resolve, 300))
+          console.log(weatherData)
+          }
+        }
+        onGetCurrentLocation={
+          async (position) => {
+            const { latitude, longitude } = position.coords;
+            const data = await getWeatherData(`${latitude},${longitude}`);
+            setWeatherData(data);
+            await new Promise(resolve => setTimeout(resolve, 300))
+            console.log(weatherData)
+          }
+        }
+        isLoading={false} 
+      />
+      <button onClick={() => {console.log(getWeatherData('Bangkok'))}} className="border-2 w-80">Click</button>
+    </div>
   )
 }
-
-export default App
--- src/components/ui/WeatherSearch.tsx
+++ src/components/ui/WeatherSearch.tsx
@@ -1,16 +1,52 @@
-import React from 'react'
-import 'tailwindcss'
+import { useState } from 'react'
 
 type WeatherSearchProps = {
   onSearch: (city: string) => void;
-  onGetCurrentLocation: () => void;
+  onGetCurrentLocation: (position:GeolocationPosition) => void;
   isLoading: boolean;
 }
 
-export const WeatherSearch = (props: WeatherSearchProps) => {
+export const WeatherSearch = ({
+  onSearch,
+  onGetCurrentLocation
+}: WeatherSearchProps) => {
+  const [searchTerm, setSearchTerm] = useState('');
   return (
-    <form>
-      <input className='m-2 border-2 w-80' placeholder='Enter your city' />
-    </form>
+    <div>
+      <form onSubmit={(event) => {
+            event.preventDefault();
+            onSearch(searchTerm.trim())
+            console.log(`Search input: ${searchTerm.trim()}`)
+          }
+        }>
+        <input 
+          type='text'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder='Enter your city' 
+          className='m-2 border-2 w-80'
+        />
+      </form>
+      <button
+        type='button'
+        onClick={
+          () => {
+            navigator.geolocation.getCurrentPosition(
+                onGetCurrentLocation, 
+                (error: GeolocationPositionError) => {
+                  console.error(error)
+                },
+                {
+                  enableHighAccuracy:true,
+                  timeout: 5000,
+                  maximumAge: 0
+                }
+              )
+          }
+        }
+      >
+        Use my current location
+      </button>
+    </div>
   )
 }
